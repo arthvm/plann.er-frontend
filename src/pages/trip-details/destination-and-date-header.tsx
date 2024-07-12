a1d5a53764745a67544084ade451a57bf63d5d4a
Arthur <90999445+arthvm@users.noreply.github.com>
2024-07-12 02:22:53 -0300

    feat: Allow user to change trip destinations and dates

--- src/pages/trip-details/destination-and-date-header.tsx
+++ src/pages/trip-details/destination-and-date-header.tsx
@@ -1,52 +1,124 @@
-import { Calendar, MapPin, Settings2 } from "lucide-react";
+import { ArrowRight, Calendar, MapPin, Settings2 } from "lucide-react";
 import { Button } from "../../components/button";
 import { useParams } from "react-router-dom";
 import { api } from "../../lib/axios";
 import { useEffect, useState } from "react";
 import { format } from "date-fns";
-
-interface Trip {
-  id: string;
-  destination: string;
-  starts_at: string;
-  ends_at: string;
-  is_confirmed: boolean;
-}
+import { ptBR } from "date-fns/locale";
+import { DateRange, DayPicker } from "react-day-picker";
+import { Modal } from "../../components/modal";
+import { Input } from "../../components/input";
 
 export function DestinationAndDateHeader() {
   const { tripId } = useParams();
-  const [trip, setTrip] = useState<Trip | undefined>();
+
+  const [isChangingTrip, setChangingTrip] = useState(false);
+  const [isDatePickerOpen, setDatePickerOpen] = useState(false);
+  const [destination, setDestination] = useState("");
+  const [eventStartAndEndDates, setEventStartAndEndDates] = useState<
+    DateRange | undefined
+  >();
 
   useEffect(() => {
-    api.get(`/trips/${tripId}`).then((response) => setTrip(response.data.trip));
+    api.get(`/trips/${tripId}`).then((response) => {
+      const trip = response.data.trip;
+
+      setDestination(trip.destination);
+      setEventStartAndEndDates({
+        from: new Date(trip.starts_at),
+        to: new Date(trip.ends_at),
+      });
+    });
   }, [tripId]);
 
-  const selectedDate = trip
-    ? format(trip.starts_at, "d' de 'LLL")
-        .concat(" ate ")
-        .concat(format(trip.ends_at, "d' de 'LLL"))
-    : null;
+  const selectedDate =
+    eventStartAndEndDates &&
+    eventStartAndEndDates.from &&
+    eventStartAndEndDates.to
+      ? format(eventStartAndEndDates.from, "d' de 'LLL")
+          .concat(" ate ")
+          .concat(format(eventStartAndEndDates.to, "d' de 'LLL"))
+      : null;
+
+  function toggleChangingTrip() {
+    setChangingTrip(!isChangingTrip);
+  }
+
+  function toggleDatePicker() {
+    setDatePickerOpen(!isDatePickerOpen);
+  }
+
+  async function changeTrip() {
+    if (!destination) return;
+    if (!eventStartAndEndDates?.from) return;
+    if (!eventStartAndEndDates?.to) return;
+
+    await api.put(`/trips/${tripId}`, {
+      destination,
+      starts_at: eventStartAndEndDates?.from,
+      ends_at: eventStartAndEndDates?.to,
+    });
+
+    toggleChangingTrip();
+  }
 
   return (
-    <div className="px-4 h-16 rounded-xl bg-zinc-900 shadow-shape flex items-center justify-between">
-      <div className="flex items-center gap-2">
-        <MapPin className="size-5 text-zinc-400" />
-        <span className="text-lg text-zing-100">{trip?.destination}</span>
-      </div>
-
-      <div className="flex items-center gap-5">
-        <div className="flex items-center gap-2">
-          <Calendar className="size-5 text-zinc-400" />
-          <span className="text-zing-100">{selectedDate}</span>
+    <>
+      <div className="px-4 h-16 rounded-xl bg-zinc-900 shadow-shape flex items-center justify-between">
+        <div className="flex flex-1 items-center gap-2">
+          <MapPin className="size-5 text-zinc-400" />
+          <Input
+            onChange={(event) => setDestination(event.target.value)}
+            disabled={!isChangingTrip}
+            type="text"
+            textSize="large"
+            placeholder="Para onde você vai?"
+            value={destination}
+          />
         </div>
 
-        <div className="w-px h-6 bg-zinc-800" />
+        <div className="flex items-center gap-5">
+          <button
+            onClick={toggleDatePicker}
+            disabled={!isChangingTrip}
+            className="flex items-center gap-2 outline-none text-left"
+          >
+            <Calendar className="size-5 text-zinc-400" />
+            <span className="text-lg text-zinc-400 min-w-40  flex-1">
+              {selectedDate ?? "Quando?"}
+            </span>
+          </button>
+          <div className="w-px h-6 bg-zinc-800" />
 
-        <Button variant="secondary">
-          Alterar local/data
-          <Settings2 className="size-5" />
-        </Button>
+          {isChangingTrip ? (
+            <Button onClick={changeTrip}>
+              Confirmar
+              <ArrowRight className="size-5" />
+            </Button>
+          ) : (
+            <Button onClick={toggleChangingTrip} variant="secondary">
+              Alterar local/data
+              <Settings2 className="size-5" />
+            </Button>
+          )}
+        </div>
       </div>
-    </div>
+
+      {isDatePickerOpen && (
+        <Modal
+          size="fit"
+          title="Selecione a data"
+          toggleModal={toggleDatePicker}
+        >
+          <DayPicker
+            selected={eventStartAndEndDates}
+            onSelect={setEventStartAndEndDates}
+            locale={ptBR}
+            disabled={{ before: new Date() }}
+            mode="range"
+          />
+        </Modal>
+      )}
+    </>
   );
 }
